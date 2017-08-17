3f69c143390f0018fdcfa9fdf3e1d176d0a98be9
Elton <user@iphone.local.valenciacc.edu>
2017-08-17 14:57:18 -0400

    fix

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("sequelize_library", "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
+// var sequelize = new Sequelize("sequelize_library", "root", "root", {
+//   host: "localhost",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
+
+var sequelize = new Sequelize(process.env.DATABASE_URL, {
+  logging: false,
+  dialectOptions: {
+    ssl: true /* for SSL config since Heroku gives you this out of the box */
   }
 });
 
